9bcbe6c31815eb176c7a3f89511752a8e85c6b7b
Jorg <jorg.vanrenterghem@energieid.be>
2026-01-20 14:11:21 +0100

    Add more examples

--- src/demo/main.py
+++ src/demo/main.py
@@ -1,4 +1,5 @@
 import json
+from src.modules.tariff.sources.entsoe_day_ahead import EntsoeDayAheadTariffSource
 from src.modules.tariff.app import TariffApp
 from src.cofy.app import Cofy
 
@@ -8,5 +9,14 @@ cofy = Cofy(settings={})
 
 tariffs = TariffApp(settings={"api_key": environment.get("ENTSOE_API_KEY", "")})
 cofy.register_module(tariffs)
+
+## Tariff app with custom source
+source = EntsoeDayAheadTariffSource(country_code="NL", api_key=environment.get("ENTSOE_API_KEY", ""))
+nl_tariffs = TariffApp(settings={"source": source, "name": "nl_tariffs"})
+cofy.register_module(nl_tariffs)
+
+## Tariff app with default source and custom settings
+fr_tariffs = TariffApp(settings={"country_code": "FR", "api_key": environment.get("ENTSOE_API_KEY", ""), "name": "fr_tariffs"})
+cofy.register_module(fr_tariffs)
 
 app = cofy.fastApi
