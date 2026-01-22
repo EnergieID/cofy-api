f15296a0ed94938e0210be64bc0f3477b5c49872
Jorg <jorg.vanrenterghem@energieid.be>
2026-01-22 15:21:22 +0100

    Test cofy api

--- tests/cofy/app_test.py
+++ tests/cofy/app_test.py
@@ -1,26 +1,7 @@
 from fastapi import FastAPI
 
 from src.cofy.app import Cofy
-from src.shared.module import Module
-
-
-class DummyModule(Module):
-    def __init__(self, name, type_, router=None):
-        self._name = name
-        self._type = type_
-        self._router = router
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def type(self):
-        return self._type
-
-    @property
-    def router(self):
-        return self._router
+from tests.cofy.dummy_module import DummyModule
 
 
 def test_cofy_initialization():
