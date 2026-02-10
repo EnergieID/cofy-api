33d0f891167a6cad352c6820b9f7597e46e7dded
Jorg <jorg.vanrenterghem@energieid.be>
2026-02-10 14:27:43 +0100

    remove unused init

--- src/modules/production/module.py
+++ src/modules/production/module.py
@@ -4,6 +4,3 @@ from src.shared.timeseries.module import TimeseriesModule
 class ProductionModule(TimeseriesModule):
     type: str = "production"
     type_description: str = "Module providing production data as time series."
-
-    def __init__(self, settings: dict, **kwargs):
-        super().__init__(settings, **kwargs)
