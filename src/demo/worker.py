85e74283ac88fdcd51e792f36296a40ee2f5cfa6
Jorg <jorg.vanrenterghem@energieid.be>
2026-02-25 16:17:36 +0100

    Fix formatting

--- src/demo/worker.py
+++ src/demo/worker.py
@@ -17,9 +17,11 @@ worker = CofyWorker(url=REDIS_URL)
 async def startup(ctx: dict) -> None:
     ctx["db_engine"] = create_engine(DB_URL)
 
+
 @worker.on_shutdown
 async def shutdown(ctx: dict) -> None:
     ctx["db_engine"].dispose()
+
 
 worker.schedule(
     sync_members_from_csv,
