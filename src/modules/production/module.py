f6b33e213dc7e51e435cc5a895fd6ef0e8c20474
Jorg <jorg.vanrenterghem@energieid.be>
2026-03-03 10:14:29 +0100

    Remove unused files

--- src/modules/production/module.py
+++
@@ -1,6 +0,0 @@
-from src.shared.timeseries.module import TimeseriesModule
-
-
-class ProductionModule(TimeseriesModule):
-    type: str = "production"
-    type_description: str = "Module providing production data as time series."
