a920dc3b52a0c0cca0d912a43d81e2df129b0bf7
Jorg <jorg.vanrenterghem@energieid.be>
2026-03-24 17:25:39 +0100

    Add arg docs to function instead of only in comment in main

--- src/cofy/modules/directive/sources/directive_source.py
+++ src/cofy/modules/directive/sources/directive_source.py
@@ -9,6 +9,13 @@ from ..formats.directive import DIRECTIVE_STEPS
 
 class DirectiveSource(TimeseriesSource):
     def __init__(self, source: TimeseriesSource, boundries: tuple[float, float, float, float], reverse: bool = False):
+        """A TimeseriesSource that maps numeric values to directive steps based on provided boundaries.
+
+        Args:
+            source: The underlying TimeseriesSource to fetch data from.
+            boundries: A tuple of four float values that define the thresholds for mapping numeric values to directive steps. The values should be in ascending order and correspond to the steps in DIRECTIVE_STEPS
+            reverse: If True, the mapping of values to directive steps will be reversed (i.e., higher values will correspond to more negative steps).
+        """
         self.source = source
         self.boundries = boundries
         self.reverse = reverse
