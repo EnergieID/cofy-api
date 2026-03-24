4db2949895fea9384340411a8a16aa8dbc5d2a5f
Jorg <jorg.vanrenterghem@energieid.be>
2026-03-24 17:31:47 +0100

    Remove duplicate specification

--- src/cofy/modules/directive/formats/directive.py
+++ src/cofy/modules/directive/formats/directive.py
@@ -1,19 +1,12 @@
 from datetime import datetime
-from enum import StrEnum
+from typing import Literal
 
 from pydantic import BaseModel
 
 from cofy.modules.timeseries import JSONFormat
 
-DIRECTIVE_STEPS = ("--", "-", "0", "+", "++")
-
-
-class DirectiveSteps(StrEnum):
-    NEGATIVE = "--"
-    SLIGHT_NEGATIVE = "-"
-    NEUTRAL = "0"
-    SLIGHT_POSITIVE = "+"
-    POSITIVE = "++"
+DirectiveSteps = Literal["--", "-", "0", "+", "++"]
+DIRECTIVE_STEPS = DirectiveSteps.__args__
 
 
 class DirectiveRecord(BaseModel):
